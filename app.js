f45496a246238029cf7e0ebb8ffb1c655fa603ac
Anas Ali <anasalimgmt@gmail.com>
2024-03-30 03:28:59 +0500

    firstupdate

---
+++ app.js
@@ -0,0 +1,69 @@
+//         CHAPTER 17 to 20
+//        QUESTION NO 1
+
+/*var emptyArray = [[],[],[]]
+document.write(emptyArray)*/
+
+//        QUESTION NO 2
+
+// var emptyArray = [
+//     [0,1,2,3], 
+//     [1,0,1,2], 
+//     [2,1,0,1]
+// ]
+// document.write(emptyArray[0] + "<br>" + emptyArray[1] + "<br>" + emptyArray[2] );
+
+// var emptyArray2 = [
+// [0,1,2,3] ,
+// [1,0,1,2],
+// [2,1,0,1]
+// ]
+// console.log(emptyArray2);
+
+//        QUESTION NO 3
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(i + "<br>")
+// }
+
+//        QUESTION NO 4
+
+// var firstValue = +prompt("enter a first digit")
+// var secondValue = +prompt("enter a second digit")
+// for (var i = 1; i <= secondValue; i++) {
+//     document.write(firstValue + " x " + i + " = " + firstValue * i + "<br>")
+// }
+
+//        QUESTION NO 5
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange", "Strawerry"]
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(` ${fruits[i]} <br> `)
+// }
+// for (var j = 0; j < fruits.length; j++) {
+//     document.write(`Element at index ${j} is ${fruits[i]}<br>`)
+// }
+
+// var cars = ["mehran", "suzuki", "fx", "khyber"]
+// var carInput = prompt("Enter a car name")
+// var match = "no"
+// for (var i = 0; i <= cars.length; i++){
+//     if(carInput === cars[i]){
+//         match ="yes"
+//         alert(carInput + "found")
+//     }
+// }
+// if(match == "no"){
+//     alert(carInput + "not found")
+// }
+
+// var heads = prompt('enter plyr name 1')
+// var heads2 = prompt('enter plyr name 2')
+// var toss = Math.random()*2
+// var convert = Math.floor(toss)
+// if(convert ==0){
+//     alert(heads + " wins")
+// }
+// else {
+//     alert(heads2 + " loser")
+// }
